52b3fda7f02c4279922a0257a987ca97feb5f43d
Dmitriy Teplinskiy <99141331+MONSMIN@users.noreply.github.com>
2023-08-23 17:00:34 +0300

    update

--- src/routes/contact.py
+++ src/routes/contact.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import APIRouter, HTTPException, Depends, status, Path, Query
+from fastapi_limiter.depends import RateLimiter
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 
@@ -17,18 +18,20 @@ router = APIRouter(prefix='/contacts', tags=["contacts"])
             dependencies=[Depends(RateLimiter(times=10, seconds=60))])
 async def read_contacts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db),
                         current_user: User = Depends(auth_service.get_current_user)):
-    contacts = await repo_contacts.get_contacts(skip, limit, current_user, db)
+    contacts = await repository_contacts.get_contacts(skip, limit, current_user, db)
     return contacts
 
 
-@router.get("/", response_model=List[ContactResponse])
+@router.get("/", response_model=List[ContactResponse], description='No more than 10 requests per minute',
+            dependencies=[Depends(RateLimiter(times=10, seconds=60))])
 async def get_all(limit: int = 10, offset: int = 0, db: AsyncSession = Depends(get_db),
                   user: User = Depends(auth_service.get_current_user)):
     contacts = await repository_contacts.get_contacts(db)
     return contacts[offset:][:limit]
 
 
-@router.get("/{contact_id}", response_model=ContactResponse)
+@router.get("/{contact_id}", response_model=ContactResponse, description='No more than 10 requests per minute',
+            dependencies=[Depends(RateLimiter(times=10, seconds=60))])
 async def get_contact(contact_id: int = Path(ge=1), db: AsyncSession = Depends(get_db),
                       user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.get_contact(contact_id, db, user)
@@ -41,14 +44,16 @@ async def get_contact(contact_id: int = Path(ge=1), db: AsyncSession = Depends(g
     return contact
 
 
-@router.post("/", response_model=ContactResponse, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=ContactResponse, description='No more than 10 requests per minute',
+             dependencies=[Depends(RateLimiter(times=10, seconds=60))], status_code=status.HTTP_201_CREATED)
 async def create_contact(body: ContactSchema, db: AsyncSession = Depends(get_db),
                          user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.create_contact(body, db, user)
     return contact
 
 
-@router.put("/{contact_id}", response_model=ContactResponse)
+@router.put("/{contact_id}", response_model=ContactResponse, description='No more than 10 requests per minute',
+            dependencies=[Depends(RateLimiter(times=10, seconds=60))])
 async def update_contact(body: ContactUpdateSchema, contact_id: int = Path(ge=1), db: AsyncSession = Depends(get_db),
                          user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.update_contact(contact_id, body, db, user)
@@ -60,7 +65,8 @@ async def update_contact(body: ContactUpdateSchema, contact_id: int = Path(ge=1)
     return contact
 
 
-@router.delete("/{contact_id}", response_model=ContactResponse)
+@router.delete("/{contact_id}", response_model=ContactResponse, description='No more than 10 requests per minute',
+               dependencies=[Depends(RateLimiter(times=10, seconds=60))])
 async def delete_contact(contact_id: int = Path(ge=1), db: AsyncSession = Depends(get_db),
                          user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.remove_contact(contact_id, db, user)
@@ -72,6 +78,8 @@ async def delete_contact(contact_id: int = Path(ge=1), db: AsyncSession = Depend
     return contact
 
 
-@router.get("/upcoming-birthdays/", response_model=List[ContactResponse])
+@router.get("/upcoming-birthdays/", response_model=List[ContactResponse],
+            description='No more than 10 requests per minute',
+            dependencies=[Depends(RateLimiter(times=10, seconds=60))])
 async def get_upcoming_birthdays(db: Session = Depends(get_db)):
     return await repository_contacts.get_upcoming_birthdays(db)
